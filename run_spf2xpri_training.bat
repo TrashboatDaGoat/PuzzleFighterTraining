7e22c4dd85ae8a465a974bb0b103a6b4010dd75e
TrashboatDaGoat <92413915+TrashboatDaGoat@users.noreply.github.com>
2022-06-17 07:26:17 -0400

    SPF2XPRI Batch file
    Added a separate batch file so you can practice with the X pri balance patch

---
+++ run_spf2xpri_training.bat
@@ -0,0 +1,5 @@
+@echo off
+echo STARTING Training Mode for SPF2T
+echo Make sure you have spf2t.zip in the 'roms' folder
+start fcadefbneo.exe spf2xpri savestates\spf2xpri_fbneo.fs %cd%\puzzlefighter\spf2t.lua
+exit
