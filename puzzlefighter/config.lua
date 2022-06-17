48a3489b38d7cd3eb57139aebc8aaa837a95916d
TrashboatDaGoat <92413915+TrashboatDaGoat@users.noreply.github.com>
2022-06-16 20:16:18 -0400

    P2 char select & No Diamond

--- puzzlefighter/config.lua
+++ puzzlefighter/config.lua
@@ -1,9 +1,9 @@
-
 local default_training_settings = {
-	infinite_time = true,
+    infinite_time = true,
     show_display = true,
     pedagogical_list = 1,
     pedagogical_integer = 0,
+    no_diamond = false,
     margin_fix = 0
   }
   
