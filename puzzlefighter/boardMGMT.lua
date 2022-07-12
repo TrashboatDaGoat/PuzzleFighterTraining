987b416a405a5a1d3442e4e4cb9ba2ffb338baad
TrashboatDaGoat <92413915+TrashboatDaGoat@users.noreply.github.com>
2022-07-11 21:13:39 -0400

    Add Piece change function

--- puzzlefighter/boardMGMT.lua
+++ puzzlefighter/boardMGMT.lua
@@ -1,24 +1,30 @@
-local pressed = false
 local function piece_change()
-local keys = input.get()
-if keys['V'] then
-  if not pressed then
-    local address = keys['7'] and 0x00FF8455 or 0x00FF8453
-    pressed = true
-    local current = memory.readbyte(address)
-    if current == 4 then
-	current = 9
-elseif current == 12 then
-  current = 1
-else
-  current = current + 1
+	local keys = joypad.get()
+	if keys['P1 Up'] then
+	local address = 0xFF8455
+	local current = memory.readbyte(address)
+		if current == 4 then 
+		current = 9 elseif 
+		current == 12 then
+		current = 1 else
+		current = current + 1
+		end
+	memory.writebyte(address, current)
+	else
+		local address = 0xFF8453
+	local current = memory.readbyte(address)
+		if current == 4 then 
+		current = 9 elseif 
+		current == 12 then
+		current = 1 else
+		current = current + 1
+		end
+	memory.writebyte(address, current)
+	
+	
+	end
 end
-    memory.writebyte(address, current)
-  end
-else
-  pressed = false
-end
-end
+
 local function all_clear_gaming()
 	local address = 0xFFAB03
 		for i = 0, 208, 16 do
@@ -30,6 +36,7 @@ local function all_clear_gaming()
 end
 boardMGMT = {
 ["all_clear_gaming"]     = all_clear_gaming,
+["piece_change"]		 = piece_change,
 }
 
 return boardMGMT
